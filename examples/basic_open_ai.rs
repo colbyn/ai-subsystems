3c3d55b4e900441c8c8c9aa371f86142773d1348
Colbyn Wadman <hello@colbyn.com>
2024-02-13 17:05:16 -0700

    update

---
+++ examples/basic_open_ai.rs
@@ -0,0 +1,39 @@
+use ai_subsystems::text_api;
+use colored::Colorize;
+
+#[tokio::main]
+async fn main() -> Result<(), text_api::client::Error> {
+    let api_key = std::fs::read_to_string("secrets/open-ai.key").unwrap();
+    let api_url = text_api::client::URL::OPEN_AI_CHAT_COMPLETIONS;
+    let prompt = text_api::xml_dsl::Prompt::open("assets/basic.prompt.liquid", "question-1").unwrap();
+    let request = prompt.request
+        .with_model("mixtral-8x7b-instruct-fp16")
+        .with_stream(true);
+        // .with_stream(true)
+        // .with_model("gpt-3.5-turbo");
+    let results = text_api::client::ApiCallBuilder::default()
+        .with_request_body(request)
+        .with_api_key(api_key)
+        .with_api_url(api_url)
+        .with_logger(text_api::client::StdOutLogger::default())
+        // .with_logger(text_api::client::FileLogger::new("logs/output.txt"))
+        .build_streaming_api_call()
+        // .build_batch_api_call()
+        .unwrap()
+        .execute()
+        .await;
+    println!("{}", "DONE".cyan());
+    match results {
+        Ok(collection) => {
+            println!("{collection:#?}");
+            if let Some(output) = collection.content(0) {
+                println!("{output}");
+            }
+        }
+        Err(x) => {
+            println!("{}", "ERROR".red());
+            println!("{x:?}");
+        }
+    }
+    Ok(())
+}
